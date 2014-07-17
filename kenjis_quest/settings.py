93db404e0868abe0d91a3d75616734ca06021110
Michael Anderton <michael.anderton15@gmail.com>
2014-07-17 16:01:53 -0600

    Added corsheaders and rest_framework to settings

--- kenjis_quest/settings.py
+++ kenjis_quest/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.public',
-    'south'
+    'south',
+    'rest_framework',
+    'corsheaders'
 )
 
 MIDDLEWARE_CLASSES = (
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,3 +85,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+    'localhost/'
+)
+CORS_ORIGIN_ALLOW_ALL = True
