988db0cf091c66ce2effec00c3515654419dcf62
Robert Clayson <jkdclayson@gmail.com>
2014-07-22 11:57:22 -0600

    Created fixtures for models using django-fixtureless

---
+++ fixtureless_creation.py
@@ -0,0 +1,13 @@
+__author__ = 'administrator'
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "kenjis_quest.settings")
+from fixtureless import Factory
+
+from apps.public.models import *
+
+objects = [Player, Item, Weapon, Skill, Character, CharacterClass, Race, Nationality, Encounter, Scenario, Chapter, Game]
+
+for object in objects:
+    factory = Factory()
+    count = 20
+    fixture = factory.create(object, count)
